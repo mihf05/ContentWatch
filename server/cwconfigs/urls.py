c43b624ea52ac809e38d1575b68f49d1715308cf
smsadat1 <smsadat788@gmail.com>
2026-05-25 20:18:07 +0600

    fix: removed extra path from api

--- server/cwconfigs/urls.py
+++ server/cwconfigs/urls.py
@@ -7,7 +7,7 @@ from contentwatch.views import RegisterView, LoginView, UserMeView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('/api/auth/register', RegisterView.as_view(), name='auth_register'),
-    path('/api/auth/login', LoginView.as_view(), name='auth_login'),
-    path('/api/auth/me', UserMeView.as_view(), name='auth_me'),
+    path('api/auth/register', RegisterView.as_view(), name='auth_register'),
+    path('api/auth/login', LoginView.as_view(), name='auth_login'),
+    path('api/auth/me', UserMeView.as_view(), name='auth_me'),
 ]

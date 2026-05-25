5e1f8dbc7c2945a5e85928253e3ce413efbdc2fe
mihf05 <fahimimran0088@gmail.com>
2026-05-26 01:46:49 +0600

    feat: implement API routing configurations for collaboration, workflows, approvals, and campaigns modules

--- server/approvals/urls.py
+++ server/approvals/urls.py
@@ -2,13 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-chain_router = DefaultRouter()
-chain_router.register(r'chains', views.ApprovalChainViewSet, basename='approval-chain')
-
-request_router = DefaultRouter()
-request_router.register(r'requests', views.ApprovalRequestViewSet, basename='approval-request')
+router = DefaultRouter()
+router.register(r'chains', views.ApprovalChainViewSet, basename='approval-chain')
+router.register(r'requests', views.ApprovalRequestViewSet, basename='approval-request')
 
 urlpatterns = [
-    path('organizations/<int:org_id>/approvals/', include(chain_router.urls)),
-    path('organizations/<int:org_id>/approvals/', include(request_router.urls)),
+    path('organizations/<int:org_id>/approvals/', include(router.urls)),
 ]
--- server/collaboration/urls.py
+++ server/collaboration/urls.py
@@ -2,21 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-activity_router = DefaultRouter()
-activity_router.register(r'activities', views.ActivityViewSet, basename='activity')
-
-comment_router = DefaultRouter()
-comment_router.register(r'comments', views.CommentViewSet, basename='comment')
-
-presence_router = DefaultRouter()
-presence_router.register(r'presence', views.PresenceViewSet, basename='presence')
+collaboration_router = DefaultRouter()
+collaboration_router.register(r'activities', views.ActivityViewSet, basename='activity')
+collaboration_router.register(r'comments', views.CommentViewSet, basename='comment')
+collaboration_router.register(r'presence', views.PresenceViewSet, basename='presence')
 
 notification_router = DefaultRouter()
 notification_router.register(r'notifications', views.NotificationViewSet, basename='notification')
 
 urlpatterns = [
-    path('organizations/<int:org_id>/', include(activity_router.urls)),
-    path('organizations/<int:org_id>/', include(comment_router.urls)),
-    path('organizations/<int:org_id>/', include(presence_router.urls)),
+    path('organizations/<int:org_id>/', include(collaboration_router.urls)),
     path('', include(notification_router.urls)),
 ]
